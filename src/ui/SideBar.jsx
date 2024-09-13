1f12870f713071212f55e3afb57b71ac3104d6cf
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-13 17:55:42 +0100

    one more thing to fix

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,42 +1,79 @@
-import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import logo from "../../Temp/assets/images/logo-large.svg";
-import { Link } from "react-router-dom";
+import houseLogo from "../../Temp/assets/images/icon-nav-overview.svg";
+import transactionsLogo from "../../Temp/assets/images/icon-nav-transactions.svg";
+import budgetLogo from "../../Temp/assets/images/icon-nav-budgets.svg";
+import recurringLogo from "../../Temp/assets/images/icon-nav-recurring-bills.svg";
+import potsLogo from "../../Temp/assets/images/icon-nav-pots.svg";
 
 function SideBar() {
-  const [isActive, setIsActive] = useState("");
-  function handleActive() {
-    setIsActive("sideBar__active");
-  }
   return (
     <div className="sideBar">
+      {/* Logo Section */}
       <div className="sideBar__logo">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" className="sideBar__logo-fill" />
       </div>
 
-      <Link
-        onClick={handleActive}
+      {/* Navigation Links */}
+      <NavLink
         to="Overview"
-        className={`sideBar__btn ${isActive}`}
+        className={({ isActive }) =>
+          `sideBar__btn ${isActive ? "sideBar__active" : ""}`
+        }
       >
+        <img src={houseLogo} alt="Overview icon" className="sideBar__icon" />
         Overview
-      </Link>
+      </NavLink>
 
-      <Link to="Transactions" className={"sideBar__btn"}>
+      <NavLink
+        to="Transactions"
+        className={({ isActive }) =>
+          `sideBar__btn ${isActive ? "sideBar__active" : ""}`
+        }
+      >
+        <img
+          src={transactionsLogo}
+          alt="Transactions icon"
+          className="sideBar__icon"
+        />
         Transactions
-      </Link>
+      </NavLink>
 
-      <Link to="Budgets" className={"sideBar__btn"}>
+      <NavLink
+        to="Budgets"
+        className={({ isActive }) =>
+          `sideBar__btn ${isActive ? "sideBar__active" : ""}`
+        }
+      >
+        <img src={budgetLogo} alt="Budgets icon" className="sideBar__icon" />
         Budgets
-      </Link>
+      </NavLink>
 
-      <Link to="Pots" className={"sideBar__btn"}>
+      <NavLink
+        to="Pots"
+        className={({ isActive }) =>
+          `sideBar__btn ${isActive ? "sideBar__active" : ""}`
+        }
+      >
+        <img src={potsLogo} alt="Pots icon" className="sideBar__icon" />
         Pots
-      </Link>
+      </NavLink>
 
-      <Link to="RecurringBills" className={"sideBar__btn"}>
+      <NavLink
+        to="RecurringBills"
+        className={({ isActive }) =>
+          `sideBar__btn ${isActive ? "sideBar__active" : ""}`
+        }
+      >
+        <img
+          src={recurringLogo}
+          alt="Recurring Bills icon"
+          className="sideBar__icon"
+        />
         Recurring Bills
-      </Link>
+      </NavLink>
     </div>
   );
 }
+
 export default SideBar;
