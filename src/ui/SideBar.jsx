1ec9d0331f320fb3ab7a649967c1988accb66dec
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-13 13:56:32 +0100

    okay

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,25 +1,33 @@
 import LinkButton from "./LinkButton";
+import logo from "../../Temp/assets/images/logo-large.svg";
 
 function SideBar() {
   return (
     <div className="sideBar">
+      <div className="sideBar__logo">
+        <img src={logo} alt="logo" />
+      </div>
+
       <LinkButton to="Overview" className={"sideBar__btn"}>
         Overview
       </LinkButton>
+
       <LinkButton to="Transactions" className={"sideBar__btn"}>
         Transactions
       </LinkButton>
+
       <LinkButton to="Budgets" className={"sideBar__btn"}>
         Budgets
       </LinkButton>
+
       <LinkButton to="Pots" className={"sideBar__btn"}>
         Pots
       </LinkButton>
+
       <LinkButton to="RecurringBills" className={"sideBar__btn"}>
         Recurring Bills
       </LinkButton>
     </div>
   );
 }
-
 export default SideBar;
