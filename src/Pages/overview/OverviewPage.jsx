4395308d7274e6a59f568e6d6a4ab1116bafd213
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-15 02:20:24 +0100

    okay

--- src/Pages/overview/OverviewPage.jsx
+++ src/Pages/overview/OverviewPage.jsx
@@ -1,5 +1,28 @@
 function OverviewPage() {
-  return <div>OverviewPage</div>;
+  return (
+    <div>
+      <div className="main">
+        <h1 className="title">Overview</h1>
+        <div className="main2">
+          <div className="current">
+            <h3>Current balance</h3>
+            <div>$4,836.00</div>
+          </div>
+
+          <div className="income">
+            <h3>Income</h3>
+            <div>$3,814.25</div>
+          </div>
+
+          <div className="expense">
+            <h3>Expense</h3>
+            <div>$1,700.50</div>
+          </div>
+        </div>
+        <div className="main3"></div>
+      </div>
+    </div>
+  );
 }
 
 export default OverviewPage;
