141303dcad2d67d3a3a20998eb2313348b03a450
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-19 22:25:43 +0100

    done

--- src/Pages/recurringBills/recurringBillsPage.jsx
+++ src/Pages/recurringBills/recurringBillsPage.jsx
@@ -3,14 +3,42 @@ import breadIcon from "../../../Temp/assets/images/avatars/savory-bites-bistro.j
 import downArrow from "../../../Temp/assets/images/icon-caret-down.svg";
 import iconCaretLeft from "../../../Temp/assets/images/icon-caret-left.svg";
 import iconCaretRight from "../../../Temp/assets/images/icon-caret-right.svg";
+import BillsIcon from "../../../Temp/assets/images/icon-recurring-bills.svg";
 
 function RecurringBillsPage() {
   return (
     <div className="w-full flex flex-col px-28 pt-28 gap-12 overflow-auto">
       <Header title="Recurring Bills" />
 
-      <div className="flex">
-        <div className="w-[30%]">heloo</div>
+      <div className="flex gap-10">
+        <div className="w-[35%] space-y-10">
+          <div className="bg-black w-full text-white rounded-xl p-10 space-y-12">
+            <img src={BillsIcon} alt="BillsIcon" className="w-16" />
+            <p className="text-3xl font-semibold">Total Bills</p>
+            <p className="text-6xl font-bold">$344.45</p>
+          </div>
+          <div className="bg-white p-6">
+            <h4 className="text-3xl font-semibold p-4">Summary</h4>
+            <div className="flex justify-between p-4 border-t-[1px]">
+              <div className="text-xl text-gray-600">Paid Bills</div>
+              <div className="text-xl text-gray-900 font-bold">
+                {"4($190.00)"}
+              </div>
+            </div>
+            <div className="flex justify-between p-4 border-t-[1px]">
+              <div className="text-xl text-gray-600">Car payment</div>
+              <div className="text-xl text-gray-900 font-bold">
+                {"4($890.00)"}
+              </div>
+            </div>
+            <div className="flex justify-between p-4 border-t-[1px]">
+              <div className="text-xl text-gray-600">Paid Bills</div>
+              <div className="text-xl text-gray-900 font-bold">
+                {"4($190.00)"}
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="flex bg-white rounded-xl w-full   p-14 flex-col">
           <div className="flex pb-10 justify-between">
             <div className="flex ">
