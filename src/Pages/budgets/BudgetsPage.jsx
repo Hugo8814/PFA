675d6d09030bdfc15e6c7f21da058b9746062ea1
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-19 16:10:56 +0100

    getting lots done SAVE

--- src/Pages/budgets/BudgetsPage.jsx
+++ src/Pages/budgets/BudgetsPage.jsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router-dom";
 import Header from "../../ui/Header";
-import Pots from "../pots/pots";
+import iconCaretRight from "../../../Temp/assets/images/icon-caret-right.svg";
+import icon from "../../../Temp/assets/images/avatars/elevate-education.jpg";
+
 import BudgetChart from "./BudgetChart";
 
 function BudgetsPage() {
@@ -17,8 +20,8 @@ function BudgetsPage() {
           </div>
           <h3 className="text-4xl font-bold p-8">Spending Summary</h3>
 
-          <div className=" grid grid-cols gird-rows-4 gap-8 p-6">
-            <div className=" p-3 w-full flex  justify-between border-l-4 border-green-600 ">
+          <div className=" grid grid-cols gird-rows-4 gap-8 p-8">
+            <div className=" p-3 w-full flex  justify-between border-l-4 border-blue-700 ">
               <p className="text-gray-800 text-3xl ">Car</p>
 
               <div className="flex items-baseline gap-2">
@@ -46,10 +49,11 @@ function BudgetsPage() {
             </div>
           </div>
         </div>
-        <div className="w-full bg-white rounded-2xl p-10">
+
+        <div className="w-full bg-white rounded-2xl p-10 h-full flex flex-col gap-8">
           <div className="flex justify-between p-4 w-full">
             <div className="flex gap-4 items-center">
-              <span className="bg-blue-800 w-8 h-8 rounded-full"></span>
+              <span className="bg-blue-700 w-8 h-8 rounded-full"></span>
               <div className="text-4xl font-bold">Car</div>
             </div>
             <button className="text-gray-900 text-4xl font-bold">...</button>
@@ -57,7 +61,47 @@ function BudgetsPage() {
 
           <p className="text-2xl p-4">Maximum of $200.00</p>
 
-          <div className="w-full bg-[#F8F4F0] h-14 rounded-md"></div>
+          <div className="w-full bg-[#F8F4F0] h-12 rounded-md ">
+            <div className="w-2/3 bg-blue-700 h-12 rounded-md"></div>
+          </div>
+
+          <div className=" my-8   w-full flex  justify-between border-l-4 border-blue-600 ">
+            <div className="  w-full  px-6">
+              <p className="text-gray-500 text-2xl  ">Spent</p>
+              <p className="text-gray-800 text-2xl font-bold ">$104</p>
+            </div>
+
+            <div className=" items-baseline gap-2 w-full h-full border-l-4 border-[#F8F4F0] px-6">
+              <p className="text-gray-500 text-2xl  ">Reamaining</p>
+              <p className="text-gray-800 text-2xl font-bold">$96</p>
+            </div>
+          </div>
+          <div className="bg-[#F8F4F0]  rounded-xl">
+            <div className="flex justify-between p-5 w-full">
+              <div className="text-3xl font-bold">Latest Spending</div>
+              <Link to="/Transactions" className="text-gray-500 text-2xl flex ">
+                See Details{" "}
+                <img
+                  src={iconCaretRight}
+                  alt="Right arrow"
+                  className="w-2 ml-4"
+                />
+              </Link>
+            </div>
+
+            <div className="flex justify-between p-6">
+              <div className="flex gap-4 items-center ">
+                <img src={icon} alt="" className="w-16 rounded-full" />
+                <p className="text-2xl font-bold">Car payment</p>
+              </div>
+              <div className="  text-end space-y-2">
+                <div className="text-2xl font-bold text-green-700">
+                  +$399.50
+                </div>
+                <div className="text-gray-500 text-xl">19 Jul 2021</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/pots/PotsPage.jsx
+++ src/Pages/pots/PotsPage.jsx
@@ -4,6 +4,143 @@ function PotsPage() {
   return (
     <div className="w-full flex flex-col px-28 pt-28 gap-12 overflow-auto">
       <Header title="Pots" btn={true} text="+Add New Pot" />
+
+      <div className="grid grid-cols-3 gap-10">
+        <div className="w-full bg-white rounded-2xl p-10 h-full flex flex-col gap-8">
+          <div className="flex justify-between p-2 w-full">
+            <div className="flex gap-4 items-center">
+              <span className="bg-blue-700 w-8 h-8 rounded-full"></span>
+              <div className="text-4xl font-bold">Car</div>
+            </div>
+            <button className="text-gray-900 text-4xl font-bold">...</button>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">
+              Total Saved
+            </p>
+            <p className="text-5xl p-2 font-bold"> $200.00</p>
+          </div>
+
+          <div className="w-full bg-[#F8F4F0] h-5 rounded-full ">
+            <div className="w-2/3 bg-blue-700 h-5 rounded-full"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">68.8%</p>
+            <p className="text-2xl p-2 ">Target of $2,000</p>
+          </div>
+
+          <div className="flex  gap-6">
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              + Add Money
+            </button>
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              Withdraw
+            </button>
+          </div>
+        </div>
+
+        <div className="w-full bg-white rounded-2xl p-10 h-full flex flex-col gap-8">
+          <div className="flex justify-between p-2 w-full">
+            <div className="flex gap-4 items-center">
+              <span className="bg-blue-700 w-8 h-8 rounded-full"></span>
+              <div className="text-4xl font-bold">Car</div>
+            </div>
+            <button className="text-gray-900 text-4xl font-bold">...</button>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">
+              Total Saved
+            </p>
+            <p className="text-5xl p-2 font-bold"> $200.00</p>
+          </div>
+
+          <div className="w-full bg-[#F8F4F0] h-5 rounded-full ">
+            <div className="w-2/3 bg-blue-700 h-5 rounded-full"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">68.8%</p>
+            <p className="text-2xl p-2 ">Target of $2,000</p>
+          </div>
+
+          <div className="flex  gap-6">
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              + Add Money
+            </button>
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              Withdraw
+            </button>
+          </div>
+        </div>
+
+        <div className="w-full bg-white rounded-2xl p-10 h-full flex flex-col gap-8">
+          <div className="flex justify-between p-2 w-full">
+            <div className="flex gap-4 items-center">
+              <span className="bg-blue-700 w-8 h-8 rounded-full"></span>
+              <div className="text-4xl font-bold">Car</div>
+            </div>
+            <button className="text-gray-900 text-4xl font-bold">...</button>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">
+              Total Saved
+            </p>
+            <p className="text-5xl p-2 font-bold"> $200.00</p>
+          </div>
+
+          <div className="w-full bg-[#F8F4F0] h-5 rounded-full ">
+            <div className="w-2/3 bg-blue-700 h-5 rounded-full"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">68.8%</p>
+            <p className="text-2xl p-2 ">Target of $2,000</p>
+          </div>
+
+          <div className="flex  gap-6">
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              + Add Money
+            </button>
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              Withdraw
+            </button>
+          </div>
+        </div>
+        <div className="w-full bg-white rounded-2xl p-10 h-full flex flex-col gap-8">
+          <div className="flex justify-between p-2 w-full">
+            <div className="flex gap-4 items-center">
+              <span className="bg-blue-700 w-8 h-8 rounded-full"></span>
+              <div className="text-4xl font-bold">Car</div>
+            </div>
+            <button className="text-gray-900 text-4xl font-bold">...</button>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">
+              Total Saved
+            </p>
+            <p className="text-5xl p-2 font-bold"> $200.00</p>
+          </div>
+
+          <div className="w-full bg-[#F8F4F0] h-5 rounded-full ">
+            <div className="w-2/3 bg-blue-700 h-5 rounded-full"></div>
+          </div>
+
+          <div className="flex justify-between items-center">
+            <p className="text-2xl text-gray-500 p-2 font-semibold">68.8%</p>
+            <p className="text-2xl p-2 ">Target of $2,000</p>
+          </div>
+
+          <div className="flex  gap-6">
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              + Add Money
+            </button>
+            <button className="bg-[#F8F4F0]  text-2xl font-bold p-6 w-full rounded-xl">
+              Withdraw
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
