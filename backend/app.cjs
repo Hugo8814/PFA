2c2759f8fc831292a97f47756a5cb23ef6ac88a8
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-09-27 22:05:00 +0100

    done, next

---
+++ backend/app.cjs
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+
+const port = 9000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api", (req, res) => {
+  const filePath = `${__dirname}/data/data.json`;
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to read file" });
+    }
+    res.setHeader("Content-Type", "application/json");
+    res.json(JSON.parse(data));
+    console.log("hi");
+  });
+});
+
+app.listen(port, () => {
+  console.log("Hello 2");
+});
--- backend/app.js
+++
@@ -1,7 +0,0 @@
-const app = require("express");
-
-const port = 9000;
-
-app.listen(port, (req, res) => {
-  console.log("hello");
-});
