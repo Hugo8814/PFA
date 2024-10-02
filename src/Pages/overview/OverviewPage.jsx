bdcb79640f1dda9668414c8246dd83410f1de1bd
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-02 15:09:53 +0100

    okay

--- src/Pages/overview/OverviewPage.jsx
+++ src/Pages/overview/OverviewPage.jsx
@@ -20,48 +20,6 @@ function OverviewPage() {
       <div className="w-full h-full flex gap-8">
         <div className="flex flex-col gap-4 w-full ">
           <PotWiget />
-
-          <div className="flex bg-white rounded-xl  flex-col p-8">
-            <SubTitle to="/transactions">Transactions</SubTitle>
-            <div className="flex justify-between p-6">
-              <div className="flex gap-4 items-center ">
-                <img src={breadIcon} alt="" className="w-16 rounded-full" />
-                <p className="text-2xl font-bold">Name</p>
-              </div>
-              <div className="space-y-2">
-                <div className="text-2xl font-bold text-green-700">
-                  +$399.50
-                </div>
-                <div className="text-gray-500 text-xl">19 Jul 2021</div>
-              </div>
-            </div>
-
-            <div className="flex justify-between p-6">
-              <div className="flex gap-4 items-center ">
-                <img src={breadIcon} alt="" className="w-16 rounded-full" />
-                <p className="text-2xl font-bold">Name</p>
-              </div>
-              <div className="space-y-2">
-                <div className="text-2xl font-bold text-green-700">
-                  +$399.50
-                </div>
-                <div className="text-gray-500 text-xl">19 Jul 2021</div>
-              </div>
-            </div>
-
-            <div className="flex justify-between p-6">
-              <div className="flex gap-4 items-center ">
-                <img src={breadIcon} alt="" className="w-16 rounded-full" />
-                <p className="text-2xl font-bold">Name</p>
-              </div>
-              <div className="space-y-2">
-                <div className="text-2xl font-bold text-green-700">
-                  +$399.50
-                </div>
-                <div className="text-gray-500 text-xl">19 Jul 2021</div>
-              </div>
-            </div>
-          </div>
         </div>
 
         <div className="w-full  flex flex-col gap-4">
---
+++ src/Pages/overview/transactionWiget.jsx
@@ -0,0 +1,41 @@
+function transactionWiget() {
+  return (
+    <div className="flex bg-white rounded-xl  flex-col p-8">
+      <SubTitle to="/transactions">Transactions</SubTitle>
+      <div className="flex justify-between p-6">
+        <div className="flex gap-4 items-center ">
+          <img src={breadIcon} alt="" className="w-16 rounded-full" />
+          <p className="text-2xl font-bold">Name</p>
+        </div>
+        <div className="space-y-2">
+          <div className="text-2xl font-bold text-green-700">+$399.50</div>
+          <div className="text-gray-500 text-xl">19 Jul 2021</div>
+        </div>
+      </div>
+
+      <div className="flex justify-between p-6">
+        <div className="flex gap-4 items-center ">
+          <img src={breadIcon} alt="" className="w-16 rounded-full" />
+          <p className="text-2xl font-bold">Name</p>
+        </div>
+        <div className="space-y-2">
+          <div className="text-2xl font-bold text-green-700">+$399.50</div>
+          <div className="text-gray-500 text-xl">19 Jul 2021</div>
+        </div>
+      </div>
+
+      <div className="flex justify-between p-6">
+        <div className="flex gap-4 items-center ">
+          <img src={breadIcon} alt="" className="w-16 rounded-full" />
+          <p className="text-2xl font-bold">Name</p>
+        </div>
+        <div className="space-y-2">
+          <div className="text-2xl font-bold text-green-700">+$399.50</div>
+          <div className="text-gray-500 text-xl">19 Jul 2021</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default transactionWiget;
