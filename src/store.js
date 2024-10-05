99240122ac9c21e4884bb69c8efd7cbd3d1b3800
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-05 21:05:49 +0100

    okay

--- src/store.js
+++ src/store.js
@@ -3,13 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import overviewReducer from "./Pages/overview/overviewSlice";
 
 import potReducer from "./Pages/pots/PotSlice";
-import addModalReducer from "./ui/addModalSlice";
+import modalReducer from "./ui/modalSlice";
 
 const store = configureStore({
   reducer: {
     overview: overviewReducer,
     pots: potReducer,
-    addModal: addModalReducer,
+    modal: modalReducer,
   },
 });
 
