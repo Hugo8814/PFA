d21929ab677f0149f7d16f1fcfafc3eb64c42a3e
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-18 12:24:31 +0100

    okay

--- src/Pages/transactions/transactionSlice.js
+++ src/Pages/transactions/transactionSlice.js
@@ -5,8 +5,8 @@ const transactionSlice = createSlice({
   initialState: {
     query: [],
     transactions: [],
+    recurringTransactions: [], // Add a new state for recurring transactions
   },
-
   reducers: {
     setTransactions(state, action) {
       state.transactions = action.payload;
@@ -14,10 +14,17 @@ const transactionSlice = createSlice({
     addTransaction(state, action) {
       state.transactions.push(action.payload);
     },
+    addRecurringTransaction(state, action) {
+      state.recurringTransactions.push(action.payload); // Handle recurring transactions
+    },
   },
 });
-export const { setTransactions, addTransaction } = transactionSlice.actions;
+
+export const { setTransactions, addTransaction, addRecurringTransaction } =
+  transactionSlice.actions;
 
 export const getTransactions = (state) => state.transactions.transactions;
+export const getRecurringTransactions = (state) =>
+  state.transactions.recurringTransactions; // Selector for recurring transactions
 
 export default transactionSlice.reducer;
