39755ce5b5602c4cd74832b8caa8546041537d72
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-30 17:06:43 +0000

    a

--- src/Pages/login/LoginPage.jsx
+++ src/Pages/login/LoginPage.jsx
@@ -42,18 +42,25 @@ function LoginPage() {
   };
 
   return (
-    <div className="flex items-center p-5 h-screen bg-[#F8F4F0]">
-      <div className="relative hidden h-full max-w-[560px] flex-1 overflow-hidden rounded-[12px] bg-grey-900 p-12 lg:flex lg:flex-col">
-        <img src={logo} alt="" className="z-20 relative w-56 mt-7" />
-        <div className="absolute inset-0 z-0 h-full w-full">
+    <div className="flex items-center p-5 h-screen bg-[#F8F4F0] max-1100:flex-col max-1100:p-0  ">
+      <div className="bg-gray-900 w-full  justify-center items-center hidden max-1100:flex ">
+        <img src={logo} alt="" className="z-20  w-76 p-8 " />
+      </div>
+      <div className="relative  h-full max-w-[560px] flex-1 overflow-hidden rounded-[12px] bg-grey-900 p-12  max-1100:hidden max-1100:p-0 flex flex-col ">
+        <img
+          src={logo}
+          alt=""
+          className="z-20 relative w-56 mt-7 max-1100:hidden"
+        />
+        <div className="absolute inset-0 z-0 h-full w-full max-1100:hidden">
           <img
             alt="login and signup illustration image"
             decoding="async"
             src={img}
-            className="absolute inset-0 h-full w-full object-cover "
+            className="absolute inset-0 h-full w-full object-cover  max-1100:hidden"
           />
         </div>
-        <div className="z-10 mt-auto flex flex-col gap-8 mb-10 text-white">
+        <div className="z-10 mt-auto flex flex-col gap-8 mb-10 text-white max-1100:hidden">
           <h2 className="text-5xl font-bold max-w-[20ch]">
             Keep track of your money and save for your future
           </h2>
@@ -64,9 +71,12 @@ function LoginPage() {
         </div>
       </div>
 
-      <div className="w-[60%] flex justify-center text-2xl">
-        <div className="bg-white p-12 rounded-xl shadow-md w-[45%] flex flex-col gap-6">
-          <h1 className="text-5xl font-bold mb-6">Login</h1>
+      <div className="w-[60%] flex justify-center items-center text-2xl  max-1100:w-full max-1100:h-full">
+        <div
+          className="bg-white p-12   rounded-xl shadow-md w-[45%] flex flex-col gap-6 max-1300:w-[60%] max-1100:w-[55%] max-1100:m-[10%]
+          max-700:w-full max-700:m-[10%] max-380:m-[3%]  "
+        >
+          <h1 className="text-5xl font-bold mb-6  max-380:text-3xl">Login</h1>
           {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <div className="mb-4">
@@ -103,12 +113,12 @@ function LoginPage() {
             </div>
             <button
               type="submit"
-              className="bg-gray-900 text-2xl hover:bg-gray-800 text-white font-bold py-6 px-4 rounded-xl focus:outline-none focus:shadow-outline w-full"
+              className="bg-gray-900 text-2xl hover:bg-gray-800 text-white font-bold py-6 px-4 rounded-xl focus:outline-none focus:shadow-outline w-full  max-380:text-xl"
             >
               Login
             </button>
           </form>
-          <div className="text-gray-600 text-2xl text-center p-3">
+          <div className="text-gray-600 text-xl text-center p-3">
             Need to create an account?{" "}
             <Link
               to="/SignUp"
