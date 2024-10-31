82356ae90114e742b4b2c2b433e138958a3abd42
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-31 13:45:37 +0000

    a

--- src/Pages/overview/MoneyTabs.jsx
+++ src/Pages/overview/MoneyTabs.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 function MoneyTabs({ current, income, expenses }) {
   return (
-    <div className="w-full grid grid-cols-3 gap-12">
-      <div className="bg-gray-900 text-white rounded-lg w-full h-44 flex flex-col p-8 gap-6">
+    <div className="w-full grid grid-cols-3 gap-12 max-500:grid-cols-1">
+      <div className="bg-gray-900 text-white rounded-xl w-full h-44 flex flex-col p-8 gap-6 max-500:rounded-2xl">
         <h3 className="text-gray-300 text-2xl ml-2">Current balance</h3>
         <div className="font-bold text-5xl">$ {current}</div>
       </div>
