1fd8fa60b0e08f0cc2c6a3218abed039e754dc82
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-31 13:46:09 +0000

    a

--- src/Pages/budgets/BudgetsPage.jsx
+++ src/Pages/budgets/BudgetsPage.jsx
@@ -32,10 +32,10 @@ function BudgetsPage() {
   }));
 
   return (
-    <div className="w-full flex flex-col px-28 pt-28 gap-12 overflow-auto">
+    <div className="w-full flex flex-col px-28 pt-28 gap-12 overflow-auto max-800:px-10">
       <Header title="Budgets" btn={true} text="+Add New Budget" budget={true} />
 
-      <div className="flex gap-8">
+      <div className="flex gap-8 max-1000:flex-col">
         <Summary
           budgetTotal={budgetTotal}
           data={data}
