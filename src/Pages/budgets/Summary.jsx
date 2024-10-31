94d07de47868e6abd739a649611479f7cdc9e1ec
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-10-31 13:45:26 +0000

    a

--- src/Pages/budgets/Summary.jsx
+++ src/Pages/budgets/Summary.jsx
@@ -6,19 +6,19 @@ function Summary({ data, budgetTotal, budgetData }) {
   const spentTotal = budgetData.reduce((total, item) => total + item.spent, 0);
 
   return (
-    <div className="flex rounded-2xl flex-col gap-6 h-fit bg-white  ">
-      <div className="relative text-center px-40  py-14 ">
+    <div className="flex rounded-2xl flex-col gap-6 h-fit bg-white max-1000:flex-row max-800:flex-col  ">
+      <div className="relative text-center px-40  py-14   max-900:px-0 max-900:mx-auto">
         <div className="text-5xl font-bold absolute  left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
           ${spentTotal}
-          <p className="text-gray-500 text-lg">
+          <p className="text-gray-500 text-lg ">
             of {formatCurrency(budgetTotal)} limit
           </p>
         </div>
         <BudgetChart data={data} />
       </div>
-      <h3 className="text-4xl font-bold p-10">Spending Summary</h3>
 
-      <div className=" grid grid-cols gird-rows-4 gap-10 p-10 h-fit">
+      <div className=" grid grid-cols gird-rows-4 gap-10 p-10 h-fit max-800:p-20">
+        <h3 className="text-4xl font-bold p-10">Spending Summary</h3>
         {budgetData &&
           budgetData.map((item, index) => (
             <div
@@ -26,7 +26,7 @@ function Summary({ data, budgetTotal, budgetData }) {
               style={{ borderColor: item.theme }}
               className=" p-3 w-full flex  justify-between border-l-4  "
             >
-              <p className="text-gray-800 text-3xl ">{item.category}</p>
+              <p className="text-gray-800 text-3xl mr-4">{item.category}</p>
 
               <div className="flex items-baseline gap-2">
                 <p className="text-3xl font-bold">
