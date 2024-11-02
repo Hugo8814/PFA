eb26abad4f5a79809d928e61ad2e4955082a76b2
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-11-02 13:23:51 +0000

    a

--- src/Pages/recurringBills/recurringBillsPage.jsx
+++ src/Pages/recurringBills/recurringBillsPage.jsx
@@ -8,7 +8,8 @@ import { useSelector } from "react-redux";
 
 import { useState } from "react";
 import { getTransactions } from "../transactions/transactionSlice";
-
+import { useMediaQuery } from "react-responsive";
+import sort from "../../../assets/images/icon-sort-mobile.svg";
 function RecurringBillsPage() {
   const data = useSelector(getTransactions);
 
@@ -51,38 +52,48 @@ function RecurringBillsPage() {
         return 0;
     }
   });
+  const isScreenSmall = useMediaQuery({ maxWidth: 600 });
+  console.log(isScreenSmall);
 
   return (
-    <div className="w-full flex flex-col px-28 pt-28 gap-12 overflow-auto">
+    <div className="w-full flex flex-col px-28 pt-28 gap-12 overflow-auto max-700:px-10 max-700:pt-14">
       <Header title="Recurring Bills" />
 
-      <div className="flex gap-10">
+      <div className="flex gap-10 max-900:flex-col">
         <TotalBills />
 
         <div className="flex bg-white rounded-xl w-full  h-full  p-14 flex-col">
-          <div className="flex pb-10 justify-between">
-            <div className="flex ">
+          <div className="flex pb-10 justify-between  max-500:pb-2 ">
+            <div className="flex h-full">
               <input
                 type="text"
                 placeholder="Search transactions"
-                className="border-gray-500 border-[1px] pr-72 pl-6 py-4 rounded-xl text-2xl "
+                className="border-gray-500 border-[1px] pr-72 pl-6 py-4 rounded-xl text-2xl max-1300:pr-0"
                 onChange={(e) => handleSearchChange(e)}
               />
             </div>
-            <div className="flex gap-6 items-center      ">
-              <p type="dropdown" className="text-2xl text-gray-600">
+            <div className="flex gap-6  h-full items-center">
+              <p
+                type="dropdown"
+                className="text-2xl text-gray-600 max-600:hidden  h-full"
+              >
                 sort by
               </p>
-              <div className="h-full relative space-y-3 text-2xl">
+              <div className="h-full relative space-y-3 text-2xl ">
+                {isScreenSmall && (
+                  <button onClick={() => handleDropdown()}>
+                    <img src={sort} alt="sort icon" />
+                  </button>
+                )}
                 <button
                   onClick={() => handleDropdown()}
-                  className="border-gray-500 border-[1px] px-6 rounded-xl h-full flex items-center gap-24  justify-between font-semibold"
+                  className="border-gray-500 border-[1px] px-6 rounded-xl h-[4.2rem] flex items-center gap-24  justify-between font-semibold max-600:hidden "
                 >
                   {sortBy}
                   <img src={downArrow} alt="" />
                 </button>
                 {isdropdownOpen && (
-                  <div className="absolute w-full bg-white rounded-xl   z-10 border-gray-500 border-[1px] ">
+                  <div className="absolute w-full bg-white rounded-xl   z-10 border-gray-500 border-[1px] max-600:w-[15rem] max-600:right-0">
                     <div
                       onClick={() => handleSortBy("A to Z")}
                       className="hover:bg-gray-200 pl-6 py-4"
